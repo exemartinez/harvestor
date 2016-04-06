74f241f8c777f2412d3b42372077e2f2d8748f2b
hernanemartinez <hernanezequielmartinez@gmail.com>
2016-04-06 17:10:40 -0300

    Mayor refurbish and overhaulling for implementation of Spring MVC framework.

---
+++ twitter-harvester/src/main/java/com/ar/twitter/harvester/OldFollowersDAO.java
@@ -0,0 +1,28 @@
+package com.ar.twitter.harvester;
+
+import java.util.List;
+
+/**
+ * <font color="#000000">This just reads a plain file and stores the followers
+ * that were already tried by the FollowersHandler class.</font>
+ * 
+ * @author hmartinez
+ * @version 1.0
+ * @created 02-may-2013 10:54:49 p.m.
+ */
+public class OldFollowersDAO {
+
+	public OldFollowersDAO() {
+
+	}
+
+	@Override
+	public void finalize() throws Throwable {
+
+	}
+
+	public List getOldFollowers() {
+		return null;
+	}
+
+}
